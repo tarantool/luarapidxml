bb3bbcf7764607e520fd0696f173c6610be3a99f
Oleg Babin <babinoleg@mail.ru>
2019-11-15 17:20:41 +0300

    Simplify rapidxml.hpp
      - Remove MVCC macroses
      - Remove using of cstdlib

--- src/rapidxml.hpp
+++ src/rapidxml.hpp
@@ -8,16 +8,8 @@
 
 // If standard library is disabled, user must provide implementations of required functions and typedefs
 #if !defined(RAPIDXML_NO_STDLIB)
-    #include <cstdlib>      // For std::size_t
     #include <cassert>      // For assert
     #include <new>          // For placement new
-#endif
-
-// On MSVC, disable "conditional expression is constant" warning (level 4). 
-// This warning is almost impossible to avoid with certain types of templated code
-#ifdef _MSC_VER
-    #pragma warning(push)
-    #pragma warning(disable:4127)   // Conditional expression is constant
 #endif
 
 ///////////////////////////////////////////////////////////////////////////
@@ -2587,10 +2579,5 @@ namespace rapidxml
 
 // Undefine internal macros
 #undef RAPIDXML_PARSE_ERROR
-
-// On MSVC, restore warnings state
-#ifdef _MSC_VER
-    #pragma warning(pop)
-#endif
 
 #endif
